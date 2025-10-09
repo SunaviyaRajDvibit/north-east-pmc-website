2c4930d7197b36907839301f5ef7ea1914796433
SunaviyaRajDvibit <sunaviyarajdvibit@gmail.com>
2025-10-09 17:23:11 +0530

    update 5 files

--- src/app/Layout/home-layout/home-layout.component.ts
+++ src/app/Layout/home-layout/home-layout.component.ts
@@ -1,12 +1,78 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-layout',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home-layout.component.html',
   styleUrl: './home-layout.component.scss'
 })
 export class HomeLayoutComponent {
 
+  roleLeftItem = [
+    {
+      type: 'image',
+      img: 'https://firebasestorage.googleapis.com/v0/b/north-east-pmc.appspot.com/o/website-images%2Frole%2Fproject.png?alt=media&token=d605452c-20d6-416a-9458-d2d331a808a9',
+      alt: 'Channelizing',
+      text: 'Channelizing'
+    },
+    {
+      type: 'image',
+      img: 'https://firebasestorage.googleapis.com/v0/b/north-east-pmc.appspot.com/o/website-images%2Frole%2Ftime-management%20(2).png?alt=media&token=427fd258-73b9-4890-98b6-4cc03dd549d8',
+      alt: 'Timelines',
+      text: 'Timelines'
+    },
+    {
+      type: 'image',
+      img: 'https://firebasestorage.googleapis.com/v0/b/north-east-pmc.appspot.com/o/website-images%2Frole%2Frupee.png?alt=media&token=64fd0577-4374-45c4-ba84-3a9a4c7b4b3d',
+      alt: 'Prevent Incorrect Charges',
+      text: 'Prevent Incorrect Charges'
+    },
+    {
+      type: 'image',
+      img: 'https://firebasestorage.googleapis.com/v0/b/north-east-pmc.appspot.com/o/website-images%2Frole%2Fdiagram%20(1).png?alt=media&token=98feb378-c6d9-4991-9fdb-f295a74ec571',
+      alt: 'Daily Progress Report',
+      text: 'Daily Progress Report'
+    },
+    {
+      type: 'image',
+      img: 'https://firebasestorage.googleapis.com/v0/b/north-east-pmc.appspot.com/o/website-images%2Frole%2Fgroup.png?alt=media&token=43186e06-00aa-4640-a5dc-49c144f4d344',
+      alt: 'Daily Manpower Report',
+      text: 'Daily Manpower Report'
+    }
+  ];
+
+  roleRighrItem = [
+    {
+      type: 'image',
+      img: 'https://firebasestorage.googleapis.com/v0/b/north-east-pmc.appspot.com/o/website-images%2Frole%2Fassessment.png?alt=media&token=a248f0de-a2ba-4f6c-a1c9-63b7a7055cfa',
+      alt: 'Quality-control',
+      text: 'Quality control'
+    },
+    {
+      type: 'image',
+      img: 'https://firebasestorage.googleapis.com/v0/b/north-east-pmc.appspot.com/o/website-images%2Frole%2Ftime-management%20(2).png?alt=media&token=427fd258-73b9-4890-98b6-4cc03dd549d8',
+      alt: 'Sitemonitoring',
+      text: 'Site monitoring'
+    },
+    {
+      type: 'image',
+      img: 'https://firebasestorage.googleapis.com/v0/b/north-east-pmc.appspot.com/o/website-images%2Frole%2Frupee.png?alt=media&token=64fd0577-4374-45c4-ba84-3a9a4c7b4b3d',
+      alt: 'NecessaryModification',
+      text: 'Necessary Modification'
+    },
+    {
+      type: 'image',
+      img: 'https://firebasestorage.googleapis.com/v0/b/north-east-pmc.appspot.com/o/website-images%2Frole%2Fdiagram%20(1).png?alt=media&token=98feb378-c6d9-4991-9fdb-f295a74ec571',
+      alt: 'Update requirements',
+      text: 'Update requirements'
+    },
+    {
+      type: 'image',
+      img: 'https://firebasestorage.googleapis.com/v0/b/north-east-pmc.appspot.com/o/website-images%2Frole%2Fgroup.png?alt=media&token=43186e06-00aa-4640-a5dc-49c144f4d344',
+      alt: 'Hustle free',
+      text: 'Hustle free'
+    }
+  ];
 }
