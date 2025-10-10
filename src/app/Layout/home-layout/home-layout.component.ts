cbc637b0ea6a3d2ea24eceebc59f6b1f33be95fd
SunaviyaRajDvibit <sunaviyarajdvibit@gmail.com>
2025-10-10 12:44:01 +0530

    update home-layout.component.html, home-layout.component.scss, home-layout.component.ts and styles.scss

--- src/app/Layout/home-layout/home-layout.component.ts
+++ src/app/Layout/home-layout/home-layout.component.ts
@@ -75,4 +75,40 @@ export class HomeLayoutComponent {
       text: 'Hustle free'
     }
   ];
+
+  projects = [
+    {
+      title: 'Building Construction',
+      description:
+        'Our post-construction services give you peace of mind knowing that we are still here for you even after.',
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/north-east-pmc.appspot.com/o/website-images%2Fproject-img%2Four-project-1-1.jpg?alt=media&token=a5766466-f285-414f-a308-cf3c5a47a0e4',
+      link: '#'
+    },
+    {
+      title: 'Architecture Design',
+      description:
+        'Creative architectural designs that blend functionality, aesthetics, and sustainability.',
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/north-east-pmc.appspot.com/o/website-images%2Fproject-img%2Four-project-2.jpg?alt=media&token=9931bd81-a6b2-4697-9a7d-917cce1c1642',
+      link: '#'
+    },
+    {
+      title: 'Building Renovation',
+      description:
+        'Enhancing existing structures with modern upgrades, efficiency, and design precision.',
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/north-east-pmc.appspot.com/o/website-images%2Fproject-img%2Four-project-3.jpg?alt=media&token=5ce7605b-618c-4877-876f-4368a8a179d2',
+      link: '#'
+    },
+    {
+      title: 'Expression',
+      description:
+        'We bring creativity and practicality together — crafting expressive designs that inspire and perform.',
+      image:
+        'https://firebasestorage.googleapis.com/v0/b/north-east-pmc.appspot.com/o/website-images%2Fproject-img%2Four-project-4.jpg?alt=media&token=d3be7353-2a11-4d3a-8a7a-6dee59d97444',
+      link: '#'
+    }
+  ];
+
 }
