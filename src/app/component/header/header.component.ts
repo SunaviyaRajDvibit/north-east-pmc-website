03f09ed76a9a9158cb87a35b1c2fbf7975ce3e37
SunaviyaRajDvibit <sunaviyarajdvibit@gmail.com>
2025-10-11 15:35:04 +0530

    update app.component.html, header.component.html, header.component.scss and header.component.ts

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule,],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isScrolled = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 50; // activates after 50px scroll
+  }
 }
